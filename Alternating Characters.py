ce545b7ce35a6b9e1d440e57fd14bcc889b5e5e1
Praveen De Silva <desilvabkp.23@uom.lk>
2024-09-13 04:59:29 +0530

    Final

--- Alternating Characters.py
+++ Alternating Characters.py
@@ -1,9 +1,22 @@
 def setTarget(value):
+    '''setiing up the target'''
     if value=='A':
         return 'B'
     return 'A'
 
 def alternatingCharacters(s):
+    '''find minimum deletions'''
     point = s[0]
     target = setTarget(point)
+    numDel = 0
 
+    for i in range(1, len(s)):
+        point = s[i]
+
+        if point==target:
+            point = target
+            target = setTarget(point)
+        else:
+            numDel += 1
+
+    return numDel
