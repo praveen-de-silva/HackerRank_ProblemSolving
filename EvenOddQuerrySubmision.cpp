8cd2bbef29139dac8a787da59cd60b6879bd3ff3
Praveen De Silva <desilvabkp.23@uom.lk>
2025-04-12 03:32:13 +0530

    Add EvenOddQuerySubmission implementation to determine even or odd based on queries

---
+++ EvenOddQuerrySubmision.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+vector<string> solve(vector<int> arr, vector<vector<int>> queries) {
+    int x, y;
+    vector<string> res;
+    for (vector<int> querry : queries) {
+        x = querry[0];
+        y = querry[1];
+        
+        if ((arr[x-1] % 2 == 0) && (((x < y) && (arr[x]!=0)) || (x == y ))) {
+            res.push_back("Even");
+        }else if (arr[x-1] == 0) {
+            res.push_back("Even");
+        }
+        
+        else {
+            res.push_back("Odd");
+        }
+    }
+    return res;
+}
